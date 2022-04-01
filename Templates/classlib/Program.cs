6be8ae4b963e1900b3164207b77202ca41e2fb17
dukc <ajieskola@gmail.com>
2022-04-01 17:41:21 +0300

    Initial commit

---
+++ Templates/classlib/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var msg = "Hello World!";
+
+            Console.WriteLine(msg);
+        }
+    }
+}
